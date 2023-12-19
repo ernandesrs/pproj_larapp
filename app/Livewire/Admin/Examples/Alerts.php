698df8f1efd11565c3e385f0b644a42dcbb3581a
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-19 17:45:30 -0400

    test emit alerts

--- app/Livewire/Admin/Examples/Alerts.php
+++ app/Livewire/Admin/Examples/Alerts.php
@@ -6,11 +6,50 @@ use Livewire\Component;
 
 class Alerts extends Component
 {
+    public $alert = [
+        'type' => 'success',
+        'float' => 0,
+        'title' => null,
+        'text' => null,
+    ];
+
     public function render()
     {
         return view('livewire..admin.examples.alerts')
             ->layout('livewire.admin.default')
             ->title('Alerts');
         ;
+    }
+
+    public function showAlert()
+    {
+        $this->alert['title'] = 'Normal alert';
+        $this->alert['text'] = 'This is a normal alert text';
+    }
+
+    public function showSessionAlert()
+    {
+        $this->alert['title'] = 'Normal session alert';
+        $this->alert['text'] = 'This is a normal session alert text';
+
+        session()->flash('alert', $this->alert);
+        $this->redirectRoute('admin.examples.alerts');
+    }
+
+    public function showFloatAlert()
+    {
+        $this->alert['title'] = 'Float alert';
+        $this->alert['text'] = 'This is a float alert text';
+        $this->alert['float'] = 1;
+    }
+
+    public function showFloatSessionAlert()
+    {
+        $this->alert['title'] = 'Float session alert';
+        $this->alert['text'] = 'This is a float session alert text';
+        $this->alert['float'] = 1;
+
+        session()->flash('alert', $this->alert);
+        $this->redirectRoute('admin.examples.alerts');
     }
 }
--- resources/views/livewire/admin/examples/alerts.blade.php
+++ resources/views/livewire/admin/examples/alerts.blade.php
@@ -1,23 +1,24 @@
 <x-admin.page icon='pie-chart-fill' title="Alerts" subtitle="See all alerts examples" :breadcrumbs="[['text' => 'Alerts', 'href' => route('admin.examples.alerts')]]">
     @slot('content')
         <div class="flex flex-wrap gap-y-10">
-            <div class="basis-full sm:basis-6/12 sm:pr-2">
-                <x-admin.section title="Alert example"
-                    subtitle="This is a example of a default alert of type success, with title and text">
+            <div class="basis-full">
+                <x-admin.section title="Alerts example" subtitle="This is a example of a alert with title and text">
                     @slot('content')
-                        <x-admin.alert type="success" title="Lorem consectetur adipisicing"
-                            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus esse excepturi porro delectus dicta!" />
-                    @endslot
-                </x-admin.section>
-            </div>
+                        <div class="flex flex-wrap gap-6">
+                            <div class="w-full border p-6">
+                                <div class="text-lg font-semibold mb-3">From session</div>
+                                @dump(session('alert'))
+                                <div class="text-lg font-semibold my-3">Local</div>
+                                @dump($this->alert)
+                            </div>
 
-            <div class="basis-full sm:basis-6/12 sm:pl-2">
-                <x-admin.section title="Alert example"
-                    subtitle="This is a example of a default alert of type danger, with title, text and close button">
-                    @slot('content')
-                        <x-admin.alert type="danger" title="Lorem consectetur adipisicing"
-                            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus esse excepturi porro delectus dicta!"
-                            closable />
+                            <x-admin.button wire:click="showAlert" text="Show alert" />
+                            <x-admin.button wire:click="showSessionAlert" text="Show session alert" />
+
+                            <x-admin.button wire:click="showFloatAlert" text="Show float alert" />
+                            <x-admin.button wire:click="showFloatSessionAlert" text="Show float session alert" />
+
+                        </div>
                     @endslot
                 </x-admin.section>
             </div>
