3040c194989f46a12b2cd71a343a283515e418f7
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-20 16:34:34 -0400

    dropdown transition

--- resources/views/components/admin/dropdown.blade.php
+++ resources/views/components/admin/dropdown.blade.php
@@ -14,12 +14,18 @@
     class="dropdown {{ $location == 'left' ? 'dropdown-left' : 'dropdown-right' }} dropdown-{{ $small ? 'small' : ($large ? 'large' : '') }}">
 
     {{-- content --}}
-    <div x-show="dropped" class="dropdown-group">
+    <div x-show="dropped" x-transition:enter="transition ease-out duration-300"
+        x-transition:enter-start="opacity-0 -translate-y-14"
+        x-transition:enter-end="opacity-100 -translate-y-0" x-transition:leave="transition ease-in duration-200"
+        x-transition:leave-start="opacity-100 -translate-y-0"
+        x-transition:leave-end="opacity-0 -translate-y-14" class="dropdown-group">
         {{ $content ?? null }}
     </div>
 
     {{-- toggler --}}
-    <x-admin.button x-on:click="dropped = !dropped" text="{{ $text ?? '' }}" :small="$small" :large="$large"
-        :prepend-icon="$location == 'left' ? 'three-dots-vertical' : null" :append-icon="$location == 'right' ? 'three-dots-vertical' : null" variant="{{ $variant ?? 'primary' }}" />
+    <div class="relative z-40">
+        <x-admin.button x-on:click="dropped = !dropped" text="{{ $text ?? '' }}" :small="$small" :large="$large"
+            :prepend-icon="$location == 'left' ? 'three-dots-vertical' : null" :append-icon="$location == 'right' ? 'three-dots-vertical' : null" variant="{{ $variant ?? 'primary' }}" />
+    </div>
 
 </div>
