da78daac19357da81cd3fc33e81211fa33376a65
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-21 19:38:50 -0400

    profile view

--- resources/views/livewire/admin/profile.blade.php
+++ resources/views/livewire/admin/profile.blade.php
@@ -1,11 +1,11 @@
 <x-admin.page title="My profile" subtitle="Manage your profile data" icon="person-circle">
     <x-slot name="content">
 
-        <x-common.tab :tabs="[
+        <x-common.tab borderless :tabs="[
             [
                 'text' => 'Basic data',
                 'icon' => 'person-lines-fill',
-                'active' => true,
+                'active' => false,
             ],
             [
                 'text' => 'Security',
@@ -17,42 +17,51 @@
                 'icon' => 'credit-card-fill',
                 'active' => false,
             ],
+            [
+                'text' => 'Danger area',
+                'icon' => 'exclamation-circle-fill',
+                'active' => true,
+            ],
         ]">
 
             <x-slot name="content1">
-                SLOT #1
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit inventore cum ratione sint unde
-                    nam, distinctio fugiat aperiam tempora esse odit vitae soluta excepturi, et eum nobis quaerat iste
-                    voluptatem.
-                </p>
+                <x-admin.section title="Your basic data" subtitle="Check and update your basic profile data" no-shadow>
+                    <x-slot name="content">
+                    </x-slot>
+                </x-admin.section>
             </x-slot>
 
             <x-slot name="content2">
-                SLOT #2
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit inventore cum ratione sint unde
-                    nam, distinctio fugiat aperiam tempora esse odit vitae soluta excepturi, et eum nobis quaerat iste
-                    voluptatem.
-                </p>
+                <x-admin.section title="Your security data" subtitle="Check and update your security data" no-shadow>
+                    <x-slot name="content">
+                    </x-slot>
+                </x-admin.section>
             </x-slot>
 
             <x-slot name="content3">
-                SLOT #3
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, voluptates.
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit inventore cum ratione sint unde
-                    nam, distinctio fugiat aperiam tempora esse odit vitae soluta excepturi, et eum nobis quaerat iste
-                    voluptatem.
-                </p>
+                <x-admin.section title="Your payment data" subtitle="Check and update your payments data" no-shadow>
+                    <x-slot name="content">
+                    </x-slot>
+                </x-admin.section>
+            </x-slot>
+
+            <x-slot name="content4">
+                <x-admin.section title="Delete account" no-shadow>
+                    <x-slot name="content">
+
+                        <div class="text-admin-danger-normal">
+                            <p class="mb-4">
+                                By clicking the button your account will be deleted permanently, this will delete
+                                everything
+                                related to it and cannot be recovered.
+                            </p>
+
+                            <x-common.button variant="danger-outlined" prepend-icon="exclamation-circle"
+                                text="I know this and I want to delete my account!" />
+                        </div>
+
+                    </x-slot>
+                </x-admin.section>
             </x-slot>
 
         </x-common.tab>
