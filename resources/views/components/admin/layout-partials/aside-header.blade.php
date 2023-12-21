2a0e75852b9fda22f9897d3d02f5c4dc7d3b82ec
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-21 14:01:44 -0400

    aside header

---
+++ resources/views/components/admin/layout-partials/aside-header.blade.php
@@ -0,0 +1,10 @@
+<div class="aside-header">
+    <div class="flex flex-wrap justify-center">
+        <x-common.thumb type="avatar" size="default" image="{{ \Auth::user()->photo }}"
+            alternative-text="{{ \Auth::user()->first_name }}" />
+
+        <div class="w-full my-2 text-center font-semibold text-lg text-gray-300">
+            <p>{{ \Auth::user()->first_name }} {{ \Auth::user()->last_name }}</p>
+        </div>
+    </div>
+</div>
--- resources/views/components/admin/layout-partials/aside.blade.php
+++ resources/views/components/admin/layout-partials/aside.blade.php
@@ -1,16 +1,7 @@
 @props(['navs'])
 
 <aside class="aside">
-    <div class="aside-header">
-        <div class="flex flex-wrap justify-center">
-            <x-common.thumb type="avatar" size="default" image="{{ \Auth::user()->photo }}"
-                alternative-text="{{ \Auth::user()->username }}" />
-
-            <div class="w-full pt-3 pb-2 text-center font-semibold text-lg text-gray-500">
-                {{ \Auth::user()->username }}
-            </div>
-        </div>
-    </div>
+    <x-admin.layout-partials.aside-header />
 
     <div class="aside-inner">
         @foreach ($navs as $nav)
