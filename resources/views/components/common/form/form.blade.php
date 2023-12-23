7a48343d9392c15a8a1115c1406049dd860d0661
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-22 22:50:15 -0400

    form component actions position

--- resources/views/components/common/form/form.blade.php
+++ resources/views/components/common/form/form.blade.php
@@ -1,9 +1,11 @@
-@props(['submitText', 'submittingText', 'noActions'])
+@props(['submitText', 'submittingText', 'noActions', 'actionsStart', 'actionsEnd'])
 
 @php
     $submitText = $submitText ?? 'Submit';
     $submittingText = $submittingText ?? 'Wait...';
     $noActions = $noActions ?? false;
+    $actionsStart = $actionsStart ?? false;
+    $actionsEnd = $actionsEnd ?? false;
 @endphp
 
 <form {{ $attributes }}>
@@ -11,7 +13,8 @@
 
     @if (!$noActions)
         <div class="flex mt-8">
-            <div class="basis-full flex justify-center items-center">
+            <div
+                class="basis-full flex {{ $actionsStart ? 'justify-start' : ($actionsEnd ? 'justify-end' : 'justify-center') }} items-center">
                 <x-common.button wire:loading.remove type="submit" prepend-icon="check-lg" text="{{ $submitText }}" />
                 <x-common.button wire:loading type="submit" text="{{ $submittingText }}" loading />
             </div>
