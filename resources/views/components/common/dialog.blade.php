62ee5cd10df7c3ee774303f034c8d5552d9d7cfb
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-24 17:42:08 -0400

    dialog start with display none

--- resources/views/components/common/dialog.blade.php
+++ resources/views/components/common/dialog.blade.php
@@ -52,7 +52,7 @@
 }" @alert_activator_clicked.window="showAlert" x-on:click="dialogWrapperClick" class="dialog"
     x-show="showWrapper" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0"
     x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in duration-300"
-    x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" id="{{ $id }}"
+    x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" id="{{ $id }}" style="display: none;"
     {{ $attributes }}>
 
     <div x-show="showContent" x-transition:enter="transition ease-out duration-300"
