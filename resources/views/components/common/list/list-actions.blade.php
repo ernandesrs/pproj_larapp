03ee5176ae437afd0f5c02db6bf783321617d460
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-24 17:50:06 -0400

    actions list: button show

--- resources/views/components/common/list/list-actions.blade.php
+++ resources/views/components/common/list/list-actions.blade.php
@@ -1,6 +1,8 @@
 @props([])
 
 <div class="flex gap-1">
+    <x-common.button text="" prepend-icon="eye-fill" variant="primary-link" small />
+
     <x-common.button text="" prepend-icon="pencil-square" variant="info-link" small />
 
     <x-common.dialog-activator controls="list_delete_item_confirmation">
