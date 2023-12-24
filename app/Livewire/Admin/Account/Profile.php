cd4333958f82dc73003e974f76aa7cd34d87eb75
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-24 10:33:12 -0400

    clear code

--- app/Livewire/Admin/Account/Profile.php
+++ app/Livewire/Admin/Account/Profile.php
@@ -9,23 +9,6 @@ use Livewire\Component;
 class Profile extends Component
 {
     /**
-     * Profile data
-     *
-     * @var array
-     */
-    public $data = [
-        'first_name' => 'User',
-        'last_name' => 'Last name',
-        'username' => 'username',
-        'gender' => null
-    ];
-
-    public $password = [
-        'password' => null,
-        'password_confirmation' => null
-    ];
-
-    /**
      * Contructor
      */
     public function __construct()
@@ -43,38 +26,5 @@ class Profile extends Component
         return view('livewire..admin.account.profile')
             ->layout('livewire.admin.layout')
             ->title('My account');
-    }
-
-    /**
-     * Update profile data
-     *
-     * @return void
-     */
-    public function updateBasicData()
-    {
-        $validated = $this->validate();
-
-        if (!\Auth::user()->update($validated['data'])) {
-            Alert::danger('Your profile data updated fails.')->float()->addAlert($this);
-            return;
-        }
-
-        Alert::success('Your profile data has been updated successfully.')->float()->addAlert($this);
-    }
-
-    /**
-     * Profile data rules
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'data' => ['array'],
-            'data.first_name' => ['required', 'string', 'max:25'],
-            'data.last_name' => ['required', 'string', 'max:50'],
-            'data.username' => ['required', 'string', 'max:25'],
-            'data.gender' => ['nullable', 'string', Rule::in('n', 'm', 'f')]
-        ];
     }
 }
