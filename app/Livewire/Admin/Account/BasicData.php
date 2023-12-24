d969e7a86f84ad69be0605c8a6fc7452c793a037
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-24 08:26:44 -0400

    update profile data(resolving pepino)

---
+++ app/Livewire/Admin/Account/BasicData.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Livewire\Admin\Account;
+
+use App\Helpers\Alert;
+use Illuminate\Validation\Rule;
+use Livewire\Component;
+
+class BasicData extends Component
+{
+    /**
+     * Profile data
+     *
+     * @var array
+     */
+    public $data = [
+        'first_name' => 'User',
+        'last_name' => 'Last name',
+        'username' => 'username',
+        'gender' => null
+    ];
+
+    /**
+     * Contructor
+     */
+    public function __construct()
+    {
+        $this->data = \Auth::user()->toArray();
+    }
+
+    /**
+     * Render view
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function render()
+    {
+        return view('livewire..admin.account.basic-data');
+    }
+
+    /**
+     * Update profile data
+     *
+     * @return void
+     */
+    public function updateBasicData()
+    {
+        $validated = $this->validate();
+
+        if (!\Auth::user()->update($validated['data'])) {
+            Alert::danger('Your profile data updated fails.')->float()->addAlert($this);
+            return;
+        }
+
+        Alert::success('Your profile data has been updated successfully.')->float()->addAlert($this);
+    }
+
+    /**
+     * Profile data rules
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'data' => ['array'],
+            'data.first_name' => ['required', 'string', 'max:25'],
+            'data.last_name' => ['required', 'string', 'max:50'],
+            'data.username' => ['required', 'string', 'max:25'],
+            'data.gender' => ['nullable', 'string', Rule::in('n', 'm', 'f')]
+        ];
+    }
+}
--- resources/views/livewire/admin/account/profile.blade.php
+++ resources/views/livewire/admin/account/profile.blade.php
@@ -36,50 +36,7 @@
                     no-border>
                     <x-slot name="content">
 
-                        <x-common.form.form wire:submit="updateBasicData" submit-text="Update profile"
-                            submitting-text="Updating profile">
-                            <x-slot name="content">
-                                <div class="flex flex-wrap gap-y-5">
-                                    <div class="basis-full sm:basis-6/12 sm:pr-2">
-                                        <x-common.form.input wire:model="data.first_name" label="First name"
-                                            error="{{ $errors->first('data.first_name') }}" />
-                                    </div>
-
-                                    <div class="basis-full sm:basis-6/12 sm:pl-2">
-                                        <x-common.form.input wire:model="data.last_name" label="Last name"
-                                            error="{{ $errors->first('data.last_name') }}" />
-                                    </div>
-
-                                    <div class="basis-full sm:basis-6/12 sm:pr-2">
-                                        <x-common.form.input wire:model="data.username" label="Username"
-                                            error="{{ $errors->first('data.username') }}" />
-                                    </div>
-
-                                    <div class="basis-full sm:basis-6/12 sm:pl-2">
-                                        <x-common.form.select wire:model="data.gender" :options="[
-                                            [
-                                                'value' => 'n',
-                                                'text' => 'Not defined',
-                                            ],
-                                            [
-                                                'value' => 'm',
-                                                'text' => 'Male',
-                                            ],
-                                            [
-                                                'value' => 'f',
-                                                'text' => 'Female',
-                                            ],
-                                        ]" label="Gender"
-                                            error="{{ $errors->first('data.gender') }}" />
-                                    </div>
-
-                                    <div class="basis-full">
-                                        <x-common.form.input wire:model="data.email" label="E-mail"
-                                            error="{{ $errors->first('data.email') }}" disabled />
-                                    </div>
-                                </div>
-                            </x-slot>
-                        </x-common.form.form>
+                        <livewire:admin.account.basic-data />
 
                     </x-slot>
                 </x-admin.section>
