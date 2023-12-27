225fcdeceb3c83e8571ec6b029f98a43362127df
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-27 09:48:22 -0400

    card component rounded

--- resources/views/components/customer/cards/card.blade.php
+++ resources/views/components/customer/cards/card.blade.php
@@ -1,6 +1,6 @@
 @props([])
 
-<div {{ $attributes->merge(['class' => 'w-full p-6 shadow-lg rounded-md']) }}>
+<div {{ $attributes->merge(['class' => 'w-full p-6 shadow-lg rounded-3xl']) }}>
     @if ($head ?? null)
         {{ $head }}
     @endif
