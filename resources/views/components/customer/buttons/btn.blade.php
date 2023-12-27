b1ba54a5252fa120cce1f60991f886ab40dd24be
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-27 17:53:25 -0400

    button component adjustments

--- resources/views/components/customer/buttons/btn.blade.php
+++ resources/views/components/customer/buttons/btn.blade.php
@@ -52,7 +52,12 @@
         'flex items-center rounded-full ' . ($outlined || $link ? '' : 'bg-gradient-to-br') . ' shadow duration-300',
 
         // sizes and quare button if text is empty
-        $small ? ('text-sm ' . empty($text) ? 'px-3 py-2' : 'px-5 py-2') : ($large ? ('text-lg ' . empty($text) ? 'px-5 py-4' : 'px-10 py-4') : 'text-base ' . (empty($text) ? 'px-4 py-3' : 'px-8 py-4')),
+        // $small ? ('text-sm ' . empty($text) ? 'px-3 py-2' : 'px-5 py-2') : ($large ? ('text-lg ' . empty($text) ? 'px-5 py-4' : 'px-10 py-4') : 'text-base ' . (empty($text) ? 'px-4 py-3' : 'px-8 py-4')),
+        // text size
+        $small ? 'text-sm' : ($large ? 'text-lg' : 'text-base'),
+
+        // padding
+        $small ? (empty($text) ? 'px-3 py-2' : 'px-5 py-2') : ($large ? (empty($text) ? 'px-5 py-4' : 'px-10 py-4') : (empty($text) ? 'px-4 py-3' : 'px-8 py-4')),
 
         // text color
         $outlined || $link ? 'text-customer-' . $variant . '-normal' : 'text-white',
