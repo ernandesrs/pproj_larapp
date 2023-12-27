e3f566a44847d83607fbe4390711386403ba53a0
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-27 09:34:47 -0400

    dir to cards components

--- resources/views/livewire/customer/home.blade.php
+++ resources/views/livewire/customer/home.blade.php
@@ -1,7 +1,12 @@
 <div class="grid grid-cols-12 gap-8">
+    <div class="col-span-12">
+        <x-customer.cards.card class="">
+            <hr>
+        </x-customer.cards.card>
+    </div>
 
     <div class="col-span-12 lg:col-span-7">
-        <x-customer.card class="overflow-hidden">
+        <x-customer.cards.card class="overflow-hidden">
             <div class="flex relative">
                 <div class="basis-full">
                     <x-customer.h1 text="H1 orem ipsum dolor sit amet, consectetur!" />
@@ -21,25 +26,25 @@
                     </div>
                 </div>
             </div>
-        </x-customer.card>
+        </x-customer.cards.card>
     </div>
 
     <div class="col-span-12 lg:col-span-5 grid grid-cols-2 gap-8">
-        <x-customer.card class="col-span-2 md:col-span-1 lg:col-span-2">
+        <x-customer.cards.card class="col-span-2 md:col-span-1 lg:col-span-2">
             <x-customer.h3 text="H3 orem ipsum dolor sit amet, consectetur!" />
             <x-customer.p muted>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus illum, culpa possimus
                 similique voluptate adipisci ad illo mollitia ut laboriosam.
             </x-customer.p>
-        </x-customer.card>
+        </x-customer.cards.card>
 
-        <x-customer.card class="col-span-2 md:col-span-1 lg:col-span-2">
+        <x-customer.cards.card class="col-span-2 md:col-span-1 lg:col-span-2">
             <x-customer.h4 text="H4 orem ipsum dolor sit amet, consectetur!" />
             <x-customer.p muted>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus illum, culpa possimus
                 similique voluptate adipisci ad illo mollitia ut laboriosam.
             </x-customer.p>
-        </x-customer.card>
+        </x-customer.cards.card>
     </div>
 
 </div>
