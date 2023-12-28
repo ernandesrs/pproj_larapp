4867902e38928b6fd7f2d8f60fca98907a96309b
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-28 11:55:45 -0400

    field component now is group allows label and error messages, and select inputs

--- resources/views/components/customer/form/field.blade.php
+++ resources/views/components/customer/form/field.blade.php
@@ -1,26 +1,49 @@
 @props([
     'type' => 'text',
+    'label' => 'Label via label prop',
+    'error' => null,
 
     'options' => [],
 ])
 
 @php
-    $baseStyle = 'w-full border border-customer-light-dark-1 py-4 px-8 rounded-full text-customer-dark-light-2';
-    $hoverStyle = '';
-    $focusStyle = '';
 
-    $fullStyle = implode(' ', [$baseStyle, $hoverStyle, $focusStyle]);
+    $wrapperStyle = 'w-full';
+
+    $fieldStyle = 'w-full border ' . (empty($error) ? 'border-customer-light-dark-2 text-gray-500' : 'border-customer-danger-normal text-customer-danger-normal') . ' px-6 py-4 rounded-xl';
+
+    if (!$attributes->has('id')) {
+        $attributes = $attributes->merge(['id' => uniqid()]);
+    }
 @endphp
 
-@if ($type == 'select')
-    <select
-        {{ $attributes->merge(['class' => $fullStyle]) }}>
-        @foreach ($options as $option)
-            <option value="{{ $option['value'] }}">{{ $option['label'] }}</option>
-        @endforeach
-    </select>
-@else
-    <input
-        type="{{ $type }}"
-        {{ $attributes->merge(['class' => $fullStyle]) }} {{ $attributes }}>
-@endif
+<div
+    {{ $attributes->only(['class'])->merge([
+        'class' => $wrapperStyle,
+    ]) }}>
+
+    {{-- label --}}
+    <label class="block mb-2 font-medium text-gray-400" for="{{ $attributes->first('id') }}">{{ $label }}</label>
+
+    {{-- field --}}
+    @if ($type == 'select')
+        <select
+            class="{{ $fieldStyle }}"
+            {{ $attributes->exceptProps(['class']) }}>
+            @foreach ($options as $option)
+                <option value="{{ $option['value'] }}">{{ $option['label'] }}</option>
+            @endforeach
+        </select>
+    @else
+        <input
+            class="{{ $fieldStyle }}"
+            type="{{ $type }}"
+            {{ $attributes->exceptProps(['class']) }}>
+    @endif
+
+    {{-- feedback --}}
+    @if (!empty($error))
+        <small class="text-xs text-customer-danger-normal block mt-1 ml-1">{{ $error }}</small>
+    @endif
+
+</div>
--- resources/views/livewire/customer/account/basic-data.blade.php
+++ resources/views/livewire/customer/account/basic-data.blade.php
@@ -3,17 +3,29 @@
     submit-text="{{ __('words.update') }} {{ strtolower(__('phrases.basic_data')) }}">
 
     <div class="grid grid-cols-12 gap-x-5 gap-y-8">
-        <x-customer.form.field class="col-span-6"
-            type="text" wire:model="data.first_name" />
+        <x-customer.form.field
+            wire:model="data.first_name"
+            label="{{ __('words.first_name') }}"
+            class="col-span-6"
+            type="text" error="{{ $errors->first('data.first_name') }}" />
 
-        <x-customer.form.field class="col-span-6"
-            type="text" wire:model="data.last_name" />
+        <x-customer.form.field
+            wire:model="data.last_name"
+            label="{{ __('words.last_name') }}"
+            class="col-span-6"
+            type="text" error="{{ $errors->first('data.last_name') }}" />
 
-        <x-customer.form.field class="col-span-6"
-            type="text" wire:model="data.username" />
+        <x-customer.form.field
+            wire:model="data.username"
+            label="{{ __('words.username') }}"
+            class="col-span-6"
+            type="text" error="{{ $errors->first('data.username') }}" />
 
-        <x-customer.form.field class="col-span-6"
-            type="select" wire:model="data.gender"
+        <x-customer.form.field
+            wire:model="data.gender"
+            label="{{ __('words.gender') }}"
+            class="col-span-6"
+            type="select"
             :options="[
                 [
                     'label' => __('words.undefined'),
@@ -27,10 +39,13 @@
                     'label' => __('words.female'),
                     'value' => 'f',
                 ],
-            ]" />
+            ]" error="{{ $errors->first('data.gender') }}" />
 
-        <x-customer.form.field class="col-span-12"
-            type="email" wire:model="data.email" disabled />
+        <x-customer.form.field
+            wire:model="data.email"
+            label="{{ __('words.email') }}"
+            class="col-span-12"
+            type="email" error="{{ $errors->first('data.email') }}" disabled />
     </div>
 
 </x-customer.form.form>
