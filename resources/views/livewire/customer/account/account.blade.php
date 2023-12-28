86667495f524c847eaff4c190e3e7b3b6a4a2e60
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-28 08:20:28 -0400

    account view

--- resources/views/livewire/customer/account/account.blade.php
+++ resources/views/livewire/customer/account/account.blade.php
@@ -1,3 +1,8 @@
-<div>
-    {{-- The whole world belongs to you. --}}
-</div>
+<x-customer.partials.page
+    title="{{ __('phrases.my_profile') }}">
+
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora odit inventore cum, illum mollitia reiciendis
+    doloremque incidunt voluptatibus voluptate consectetur modi consequuntur deleniti fugit harum similique dicta
+    adipisci ducimus quis?
+
+</x-customer.partials.page>
