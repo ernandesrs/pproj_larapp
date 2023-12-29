7748ca1cf75cdc96dd66c3f062dcb53fc24a4812
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-29 18:21:29 -0400

    form component using wire:loading.class

--- resources/views/components/customer/form/form.blade.php
+++ resources/views/components/customer/form/form.blade.php
@@ -12,11 +12,15 @@
     </div>
 
     <div class="mt-8 flex justify-center">
-        <x-customer.buttons.btn wire:loading.remove type="submit" prepend-icon="check-lg" text="{{ $submitText }}"
+        <x-customer.buttons.btn
+            type="submit"
+            wire:target="{{ $action }}"
+            wire:loading.class="loading"
+
+            prepend-icon="check-lg"
+            text="{{ $submitText }}"
             variant="primary"
             no-transform />
-        <x-customer.buttons.btn wire:loading loading prepend-icon="check-lg" text="{{ $submittingText }}"
-            variant="primary" no-transform />
     </div>
 
 </form>
