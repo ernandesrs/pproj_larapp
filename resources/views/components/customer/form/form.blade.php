12581e05d40bc042857c21f1c8a082daa5e59322
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-28 21:06:13 -0400

    loading button

--- resources/views/components/customer/form/form.blade.php
+++ resources/views/components/customer/form/form.blade.php
@@ -2,6 +2,7 @@
     'action' => null,
 
     'submitText' => __('words.submit'),
+    'submittingText' => __('words.wait'),
 ])
 
 <form wire:submit="{{ $action }}">
@@ -11,8 +12,11 @@
     </div>
 
     <div class="mt-8 flex justify-center">
-        <x-customer.buttons.btn type="submit" prepend-icon="check-lg" text="{{ $submitText }}" variant="primary"
+        <x-customer.buttons.btn wire:loading.remove type="submit" prepend-icon="check-lg" text="{{ $submitText }}"
+            variant="primary"
             no-transform />
+        <x-customer.buttons.btn wire:loading loading prepend-icon="check-lg" text="{{ $submittingText }}"
+            variant="primary" no-transform />
     </div>
 
 </form>
