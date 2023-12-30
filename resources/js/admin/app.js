1ea4dc827eebfc2be65b37e2bad721bd02825c60
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-29 23:47:26 -0400

    start new admin dashboard

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -1,21 +1,3 @@
-const sidebar = {
-    sidebarEl: null,
-    sidebarTogglerEl: null,
+import '../bootstrap';
 
-    start() {
-        this.sidebarEl = document.querySelector('#jsSidebar');
-        this.sidebarTogglerEl = document.querySelector('#jsSidebarToggler');
-
-        if (this.sidebarEl && this.sidebarTogglerEl) {
-            this.startMonitor();
-        }
-    },
-
-    startMonitor() {
-        this.sidebarTogglerEl.addEventListener('click', (event) => {
-            this.sidebarEl.classList.toggle('layout-left-side-show');
-        });
-    }
-};
-
-sidebar.start();
+console.log('admin js');
--- resources/views/components/common/form/file.blade.php
+++
@@ -1,4 +0,0 @@
-@props(['as', 'uploadButtonText', 'uploadText', 'error'])
-
-<x-common.form.input type="file" upload-button-text="{{ $uploadButtonText ?? null }}"
-    upload-text="{{ $uploadText ?? null }}" error="{{ $error ?? null }}" {{ $attributes }} />
