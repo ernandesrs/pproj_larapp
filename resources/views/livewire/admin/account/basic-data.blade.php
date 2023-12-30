3fab3fb209292a3e4e54171aa6aa3f8cf4a09bee
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-29 23:51:02 -0400

    start new admin dashboard

--- resources/views/livewire/admin/account/basic-data.blade.php
+++
@@ -1,8 +0,0 @@
-<x-common.form.form wire:submit="updateBasicData" submit-text="{{ __('words.update') }}"
-    submitting-text="{{ __('words.updating') }}">
-    <x-slot name="content">
-
-        <x-common.view-partials.user-basic-data />
-
-    </x-slot>
-</x-common.form.form>
--- resources/views/livewire/admin/account/password.blade.php
+++
@@ -1,8 +0,0 @@
-<x-common.form.form wire:submit="update" submit-text="{{ __('words.update') }}"
-    submitting-text="{{ __('words.updating') }}">
-    <x-slot name="content">
-
-        <x-common.view-partials.user-password />
-
-    </x-slot>
-</x-common.form.form>
