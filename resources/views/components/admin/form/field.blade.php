354f49407c82a3ec1ec6df33eb88690ce436d1ca
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-30 22:59:19 -0400

    admin component: form field allows label

--- resources/views/components/admin/form/field.blade.php
+++ resources/views/components/admin/form/field.blade.php
@@ -1,17 +1,25 @@
 @props([
     'type' => 'text',
+    'label' => null,
     'options' => [],
 ])
 
 @php
-
     $fieldWrapperStyles = 'w-full';
 
-    $fieldStyles = 'w-full border px-4 py-4 bg-admin-light-light-2 outline-none duration-300 disabled:opacity-40 focus:bg-admin-white focus:shadow dark:border-admin-dark-normal dark:bg-admin-dark-normal';
+    $fieldStyles = 'w-full border px-4 py-4 bg-admin-white outline-none duration-300 disabled:opacity-40 focus:bg-admin-white focus:shadow dark:border-admin-dark-normal dark:bg-admin-dark-normal';
+
+    if (!$attributes->has('id')) {
+        $attributes = $attributes->merge(['id' => uniqid()]);
+    }
 
 @endphp
 
 <div {{ $attributes->only(['class'])->merge(['class' => $fieldWrapperStyles]) }}>
+
+    @if (!empty($label))
+        <label class="block font-normal text-admin-dark-light-2 text-opacity-50 dark:text-admin-light-dark-2 dark:text-opacity-75 mb-2" for="{{ $attributes->get('id') }}">{{ $label }}</label>
+    @endif
 
     @if ($type == 'select')
         <select
--- resources/views/livewire/admin/account/basic-data.blade.php
+++ resources/views/livewire/admin/account/basic-data.blade.php
@@ -2,6 +2,53 @@
     action="updateBasicData"
     submitText="{{ __('words.update') }} {{ __('words.profile') }}">
 
+    <div class="grid grid-cols-12 gap-6 py-4">
 
+        <x-admin.form.field
+            class="col-span-12 sm:col-span-6"
+            label="{{ __('words.first_name') }}"
+            wire:model="data.first_name"
+            type="text" />
+
+        <x-admin.form.field
+            class="col-span-12 sm:col-span-6"
+            label="{{ __('words.last_name') }}"
+            wire:model="data.last_name"
+            type="text" />
+
+        <x-admin.form.field
+            class="col-span-12 sm:col-span-6"
+            label="{{ __('words.username') }}"
+            wire:model="data.username"
+            type="text" />
+
+        <x-admin.form.field
+            class="col-span-12 sm:col-span-6"
+            label="{{ __('words.gender') }}"
+            wire:model="data.gender"
+            type="select"
+            :options="[
+                [
+                    'label' => __('words.undefined'),
+                    'value' => 'n',
+                ],
+                [
+                    'label' => __('words.male'),
+                    'value' => 'm',
+                ],
+                [
+                    'label' => __('words.female'),
+                    'value' => 'f',
+                ],
+            ]" />
+
+        <x-admin.form.field
+            class="col-span-12"
+            label="{{ __('words.email') }}"
+            wire:model="data.email"
+            type="email"
+            disabled />
+
+    </div>
 
 </x-admin.form.form>
