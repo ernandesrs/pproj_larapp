356864ea5a756b586060a7de1fb9f5d82233fe7d
Ernandes R Souza <ernandesrsouza@gmail.com>
2023-12-30 23:28:25 -0400

    admin global alert

--- resources/views/components/admin/layout/main.blade.php
+++ resources/views/components/admin/layout/main.blade.php
@@ -1,6 +1,9 @@
 @props([])
 
 <main class="overflow-y-auto" style="height: calc(100vh - 4rem)">
+
+    <x-common.alert closable />
+
     <div class="container min-h-full flex">
         {{ $slot }}
     </div>
