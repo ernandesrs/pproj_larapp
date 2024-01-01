ee1c9680666d1876e6ac311a71fe76dd4acdef9c
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-01-01 16:04:53 -0400

    using enums to define permission names for users

---
+++ app/Enums/PermissionsEnum.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Enums;
+
+enum PermissionsEnum: string
+{
+    /**
+     * 
+     * USER
+     * 
+     */
+    case LIST_USERS = 'list_users';
+    case SHOW_USERS = 'show_users';
+    case CREATE_USERS = 'create_users';
+    case EDIT_USERS = 'edit_users';
+    case DELETE_USERS = 'delete_users';
+    case PROMOTE_USERS = 'promote_users';
+    case DEMOTE_USERS = 'demote_users';
+
+    /**
+     * Label
+     *
+     * @return string
+     */
+    public function label()
+    {
+        return match ($this) {
+            static::LIST_USERS => __('admin/phrases.list_users'),
+            static::SHOW_USERS => __('admin/phrases.show_users'),
+            static::CREATE_USERS => __('admin/phrases.create_users'),
+            static::EDIT_USERS => __('admin/phrases.edit_users'),
+            static::DELETE_USERS => __('admin/phrases.delete_users'),
+            static::PROMOVE_USERS => __('admin/phrases.promove_users'),
+            static::DEMOTE_USERS => __('admin/phrases.demote_users'),
+        };
+    }
+}
--- resources/lang/pt_BR/admin/phrases.php
+++ resources/lang/pt_BR/admin/phrases.php
@@ -2,5 +2,13 @@
 
 return [
     'manage_users' => 'Gerencie todos os usuários cadastrados no sistema',
-    'manage_user' => 'Gerencie os dados do usuário atual'
+    'manage_user' => 'Gerencie os dados do usuário atual',
+
+    'list_users' => 'Listar usuários',
+    'show_users' => 'Mostrar usuários',
+    'create_users' => 'Criar usuários',
+    'edit_users' => 'Editar usuários',
+    'delete_users' => 'Excluir usuários',
+    'promote_users' => 'Promover usuários',
+    'demote_users' => 'Rebaixar usuários',
 ];
