70433d3cfedb6d7aaf66acfcdbe1757773b97ca1
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-01-01 12:37:49 -0400

    users migration fulltext

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,9 @@ return new class extends Migration {
             $table->string('photo')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->fullText(['first_name', 'last_name', 'username', 'email']);
+
             $table->rememberToken();
             $table->timestamps();
         });
