ca5a15645d08965e2b7445e0b6424117f23c48a2
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-01-02 16:10:30 -0400

    admin home: start cards compnents

---
+++ resources/views/components/admin/cards/dash-card.blade.php
@@ -0,0 +1,5 @@
+@props([])
+
+<div {{ $attributes->merge(['class' => 'bg-admin-primary-normal p-5']) }}>
+    fja
+</div>
--- resources/views/livewire/admin/home.blade.php
+++ resources/views/livewire/admin/home.blade.php
@@ -1,8 +1,29 @@
-<x-admin.layout.page
-    icon="pie-chart-fill"
-    title="Overview"
-    subtitle="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi similique a temporibus.">
+<x-admin.layout.page>
 
-    <h1>HOME</h1>
+    <div class="grid grid-cols-12 gap-6">
+
+        <x-admin.section
+            class="col-span-12">
+            <div class="grid grid-cols-12 gap-6">
+                <x-admin.cards.dash-card
+                    class="col-span-4"></x-admin.cards.dash-card>
+
+                <x-admin.cards.dash-card
+                    class="col-span-4"></x-admin.cards.dash-card>
+
+                <x-admin.cards.dash-card
+                    class="col-span-4"></x-admin.cards.dash-card>
+            </div>
+        </x-admin.section>
+
+        <x-admin.section
+            class="col-span-12 md:col-span-5 lg:col-span-4">
+        </x-admin.section>
+
+        <x-admin.section
+            class="col-span-12 md:col-span-7 lg:col-span-8">
+        </x-admin.section>
+
+    </div>
 
 </x-admin.layout.page>
