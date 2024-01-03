f6794fef2f2941225e7fb157ff2bd8c901733f11
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-01-03 13:27:17 -0400

    making commando to start app

---
+++ app/Console/Commands/LaraApp.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class LaraApp extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app {--fresh : Exceute migrate:fresh }';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Start application';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $importantSeeders = [
+            'RolesAndPermissionsSeeder'
+        ];
+
+        $superUserData = $this->getValidSuperAdminData();
+        $seedUsers = $this->choice('Seed users?', ['no', 'yes']);
+
+        if ($seedUsers == 'yes') {
+            $importantSeeders = [
+                ...$importantSeeders,
+                'UserSeeder'
+            ];
+        }
+
+        if ($this->option('fresh')) {
+            $this->call('migrate:fresh');
+            $this->info('Database cleared!');
+        }
+
+        // Execute seeders
+        foreach ($importantSeeders as $seeder) {
+            $this->call('db:seed', [
+                '--class' => $seeder
+            ]);
+        }
+
+        $this->info('Basic data generated!');
+
+        $this->registerSuperUser($superUserData);
+    }
+
+    /**
+     * Get a valid super admin user data
+     *
+     * @return array
+     */
+    private function getValidSuperAdminData()
+    {
+        do {
+            $this->line('');
+            $this->line('');
+            $this->line("Let's register a super admin user:");
+
+            $super['email'] = $this->ask('Enter a valid email:');
+            $super['password'] = $this->ask('Enter a password');
+            $super['password_confirmation'] = $this->ask('Confirm the password');
+
+            if ($this->validateSuperUserData($super)->fails()) {
+                $this->error('Enter a valid email and password!');
+            }
+        } while ($this->validateSuperUserData($super)->fails());
+
+        return $super;
+    }
+
+    /**
+     * Validate super user data
+     *
+     * @param array $data
+     * @return \Illuminate\Validation\Validator
+     */
+    private function validateSuperUserData(array $data)
+    {
+        return \Validator::make($data, [
+            'email' => 'required|email',
+            'password' => 'required|confirmed'
+        ]);
+    }
+
+    /**
+     * Register super user
+     *
+     * @param array $data
+     * @return void
+     */
+    private function registerSuperUser(array $data)
+    {
+        $superRole = \Spatie\Permission\Models\Role::findByName(\App\Enums\RolesEnum::SUPER_USER->value);
+        $superUser = \App\Services\UserService::create([
+            'first_name' => 'Super',
+            'last_name' => 'User',
+            'username' => 'superuser',
+            'gender' => 'n',
+            ...$data
+        ]);
+
+        $superUser->assignRole($superRole);
+
+        $this->info('Super user created!');
+    }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Enums\PermissionsEnum;
-use App\Enums\RolesEnum;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,24 +12,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        $super = \App\Models\User::factory()->create([
-            'first_name' => 'Super',
-            'last_name' => 'User',
-            'username' => 'superuser',
-            'email' => env('APP_SUPER_USER_EMAIL', 'super@mail.com'),
-        ]);
-
-        $super->assignRole(RolesEnum::SUPER_USER);
-
-        $admin = \App\Models\User::factory()->create([
-            'first_name' => 'Admin',
-            'last_name' => 'User',
-            'username' => 'adminuser',
-            'email' => 'admin@mail.com',
-        ]);
-
-        $admin->assignRole(RolesEnum::ADMIN_USER);
-
         \App\Models\User::factory(100)->create();
     }
 }
